1347f7458711afc45da886809a0d9830db57f8cb
foureverhh <foureverhh@gmail.com>
2019-01-27 21:04:31 +0100

    Added startPhoneNumberVerificaton funciton to set up phone number verification and defined verification callback to reponse to the verification result

--- app/src/main/java/com/example/zfgg04/whatsappclone/MainActivity.java
+++ app/src/main/java/com/example/zfgg04/whatsappclone/MainActivity.java
@@ -1,16 +1,29 @@
 package com.example.zfgg04.whatsappclone;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText mPhoneNumber, Code;
     private Button mSend;
+
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +35,54 @@ public class MainActivity extends AppCompatActivity {
         mPhoneNumber = findViewById(R.id.phoneNumber);
         Code = findViewById(R.id.code);
         mSend = findViewById(R.id.send);
+
+        sendButtonIsClicked();
+
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+            @Override
+            public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
+                signInWithPhoneAuthCredential(phoneAuthCredential);
+            }
+
+            @Override
+            public void onVerificationFailed(FirebaseException e) {
+
+            }
+        };
     }
 
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential phoneAuthCredential) {
+        FirebaseAuth.getInstance().signInWithCredential(phoneAuthCredential).addOnCompleteListener(
+                this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            userIsLoggedIn();
+                        }
+                    }
+                }
+        );
+    }
+
+    private void userIsLoggedIn() {
+
+    }
+
+    private void sendButtonIsClicked() {
+        mSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startPhoneNumberVerification();
+            }
+        });
+    }
+
+    private void startPhoneNumberVerification() {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                mPhoneNumber.getText().toString(),
+                60,
+                TimeUnit.SECONDS,
+                this,
+                mCallbacks);
+    }
 }
