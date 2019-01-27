b142535cd705c810c20a54c6760153a9244635c9
foureverhh <foureverhh@gmail.com>
2019-01-27 20:29:33 +0100

    Created instances of Edittext and button and Firebase

--- app/src/main/java/com/example/zfgg04/whatsappclone/MainActivity.java
+++ app/src/main/java/com/example/zfgg04/whatsappclone/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.zfgg04.whatsappclone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.FirebaseApp;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mPhoneNumber, Code;
+    private Button mSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseApp.initializeApp(this);
+
+        mPhoneNumber = findViewById(R.id.phoneNumber);
+        Code = findViewById(R.id.code);
+        mSend = findViewById(R.id.send);
     }
+
 }
